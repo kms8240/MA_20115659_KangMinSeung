873e064ece5457fdc1083523bbc97f42d0463a16
kms8240 <379gs@naver.com>
2015-12-02 22:00:57 +0900

    4번

--- app/src/main/java/com/example/subin/ma_20115659_kangminseung/wine_cellar.java
+++ app/src/main/java/com/example/subin/ma_20115659_kangminseung/wine_cellar.java
@@ -1,65 +1,31 @@
 package com.example.subin.ma_20115659_kangminseung;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class wine_cellar extends AppCompatActivity {
 
-    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wine_cellar);
 
-        listView = (ListView)this.findViewById(R.id.listView);
-
-        ArrayList<String> items = new ArrayList<>();
-        items.add("  Save");
-        items.add("  List");
-        items.add("  Back");
-
-        CustomAdapter adapter = new CustomAdapter(this, 0, items);
-        listView.setAdapter(adapter);
     }
 
-    private class CustomAdapter extends ArrayAdapter<String>{
-        private ArrayList<String> items;
+    public void onSaveButtonClicked(View v){
 
-        public CustomAdapter(Context context, int textViewResourceId, ArrayList<String> objects){
-            super(context, textViewResourceId, objects);
-            this.items = objects;
-        }
-
-        public View getView(int position, View convertView, ViewGroup patent){
-            View v = convertView;
-            if(v==null){
-                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.listview_item, null);
-            }
-
-            ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
-
-            if("  Save".equals(items.get(position)))
-                imageView.setImageResource(R.drawable.save);
-
-            TextView textView = (TextView)v.findViewById(R.id.textView);
-
-            textView.setText(items.get(position));
-
-            return v;
-        }
     }
+
+    public void onListButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), wine_list.class);
+        startActivity(intent);
+    }
+
+
+
 
 
 }
